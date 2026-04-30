0e393cd33308d06aab1cbc144ad5811a8fb30498
Evchap <evasekina906@gmail.com>
2026-04-30 16:04:41 +0300

    Nichego

--- бомбер пацанчики.py
+++ бомбер пацанчики.py
@@ -1,2 +1,28 @@
+import arcade
 
-print(2)
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+SCREEN_TITLE = "Arcade Game"
+
+
+class Game(arcade.Window):
+
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+
+    def on_draw(self):
+        self.clear()
+
+    def on_update(self, delta_time: float):
+        pass
+
+window = Game()
+arcade.run()
+
+"""
+Если в субботу получится, то свяжитесь с Паштетом, чтобы позаниматься
+
+1. ДОСТАТЬ ВСЕ каРТИноЧКИ из плАншеТика (спРайтИки)
+2. Добавить двух  персонажа в игрулю и обучить её или его или оно ходить
+
+"""
