a872588d8c6a3cf67bf0be452369d7d029dda1b8
Evchap <evasekina906@gmail.com>
2026-05-14 16:02:44 +0300

    Nichego

--- бомбер пацанчики.py
+++ бомбер пацанчики.py
@@ -3,10 +3,10 @@ import arcade
 
 
 SCREEN_TITLE = "Бомбермания"
-ROW = 20
-COLUMN = 25
-BLOCK_WIDTH = 50
-BLOCK_HEIGHT = 50
+ROW = 8
+COLUMN = 16
+BLOCK_WIDTH = 100
+BLOCK_HEIGHT = 100
 SCREEN_WIDTH = COLUMN * BLOCK_WIDTH
 SCREEN_HEIGHT = ROW * BLOCK_HEIGHT
 
@@ -64,7 +64,10 @@ class Bobo(arcade.Sprite):
 
 
 
+class Nelomoka (arcade.Sprite):
 
+    def __init__(self):
+        super().__init__('блокнет.png', scale=2)
 
 
 
@@ -75,7 +78,7 @@ class Bobo(arcade.Sprite):
 class Kambodsha (arcade.Sprite):
 
     def __init__(self):
-        super().__init__('Kam2.png', scale=0.4)
+        super().__init__('Kam2.png', scale=0.2)
         self.center_x = 1200
         self.center_y = SCREEN_HEIGHT / 3
     def update(self):
@@ -86,7 +89,7 @@ class Kambodsha (arcade.Sprite):
 class Tailand (arcade.Sprite):
 
     def __init__(self):
-        super().__init__('Tai2.png', scale=0.4)
+        super().__init__('Tai2.png', scale=0.25)
         self.center_x = 1000
         self.center_y = SCREEN_HEIGHT / 2
     def update(self):
@@ -101,17 +104,29 @@ class Tailand (arcade.Sprite):
 class Game(arcade.Window):
     def __init__(self):
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE,fullscreen=False)
+        print(self.width, self.height)
         self.Kam = Kambodsha()
         self.Tai = Tailand()
-        self.set_mouse_visible(False)
         self.fu = arcade.load_texture("фончпек.png")
         self.bumibut = arcade.SpriteList()
         self.bumibuk = arcade.SpriteList()
         self.ogot = arcade.SpriteList()
         self.ogok = arcade.SpriteList()
-        self.fonch = arcade.SpriteList()
         self.bd = arcade.SpriteList()
         self.bn = arcade.SpriteList()
+
+    def on_mouse_press(self, x: int, y: int, button: int, modifiers: int):
+        kletochka_index_x = x // BLOCK_WIDTH
+        kletochka_index_y = y // BLOCK_HEIGHT
+        kletochka_center_x = kletochka_index_x * BLOCK_WIDTH + BLOCK_WIDTH // 2
+        kletochka_center_y = kletochka_index_y * BLOCK_HEIGHT + BLOCK_HEIGHT // 2
+
+        print(kletochka_index_x,kletochka_index_y,kletochka_center_x,kletochka_center_y)
+
+        pup = Nelomoka()
+        pup.center_x = kletochka_center_x
+        pup.center_y = kletochka_center_y
+        self.bn.append(pup)
 
 
     def on_key_press(self, symbol: int, modifiers: int):
@@ -138,7 +153,7 @@ class Game(arcade.Window):
         if symbol == arcade.key.R:
             self.Tai.change_x = 10
         if symbol == arcade.key.L:
-            self.Tai.change_x = -10
+            self.Tai.change_x = -0
         if symbol == arcade.key.U:
             self.Tai.change_y = 10
         if symbol == arcade.key.D:
@@ -174,7 +189,8 @@ class Game(arcade.Window):
         self.clear()
         for y in range(ROW):
             for x in range(COLUMN):
-                arcade.draw_texture_rectangle(x * BLOCK_WIDTH +BLOCK_WIDTH/2, y * BLOCK_HEIGHT+BLOCK_HEIGHT/2, BLOCK_WIDTH, BLOCK_HEIGHT,self.fu)
+                arcade.draw_texture_rectangle(x * BLOCK_WIDTH + BLOCK_WIDTH / 2, y * BLOCK_HEIGHT+BLOCK_HEIGHT/2, BLOCK_WIDTH, BLOCK_HEIGHT,self.fu)
+                # arcade.draw_rectangle_outline(x * BLOCK_WIDTH + BLOCK_WIDTH / 2, y * BLOCK_HEIGHT*BLOCK_HEIGHT/2, BLOCK_WIDTH, BLOCK_HEIGHT, color=arcade.color.BLACK)
         self.Kam.draw()
         self.Tai.draw()
         arcade.draw_text("Выход = taб,Управление Фиолетовой чучундрой через стрелочки,рыжей - urld", 100, 100,
@@ -185,7 +201,6 @@ class Game(arcade.Window):
         self.bumibuk.draw()
         self.ogot.draw()
         self.ogok.draw()
-        self.fonch.draw()
         self.bd.draw()
         self.bn.draw()
 
@@ -204,19 +219,14 @@ arcade.run()
 
 """
 
-
-2. В окне просто отрисовать несколько надписей (хпшки первого игрока и второого) оки
-3. Погулять сегодня оки 
-4. Найти звуки для взрыва и можно на задний фон (по желанию), звуки ходьбы (по желнию)
 Звуик НЕ СКАЧИВАТЬ БЕЗ ПАШТЕ
 
 1. Узнать как выключить компудатор на питоне, нужен маленький кодик (две строчки) и если его запустить, то
 компухтер выключится. Если человек нажало на кномпу ентер, то вырубить ПК
-
  if symbol == arcade.key.ENTER:
     self.close()
 
-тадаамТА,сделаем это вместе :)) оке
+ТА,сделаем это вместе :)) оке
 -------------------------------
 Погулять
 1. По нажатию кнопки - выключать Пк
@@ -224,4 +234,17 @@ arcade.run()
 Бусты: для скорости, для бомбчек (кол-во их увеличивается), для невидимости, для увеличения кол-во огней
  нарисовать как угодно, например, в виде бутылочек (и  на них там будет нарисовано что-то ) или другой какой-то пример
  
+ 
+ 
+ 
+ 
+  self.fonch = arcade.SpriteList()
+  
+import  os
+os.system("shutdown /s /t 0")
+ 
+ -------------------------
+ 1. когда мы ставим бомбочку - изменить её размер, чтобы она соответствовала одной клеточке + огонь тоже скейл поменять
+ 2. сделать так, чтобы игроки не могли выйти за пределы окна (уже делали раньше, подсмотри в прошлые проектики :))
+ 3. сделать так, чтобы при нажатии на кнопочку ентер - просто выключался компьютер :)
 """
